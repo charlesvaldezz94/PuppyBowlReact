9f19a91ae4779d2140b27ea1bb81638f659668e5
Grant <grant.mc.foster@gmail.com>
2022-10-13 14:35:09 -0500

    set up myPuppies prop and mapped objects to puppy, displayed specific data in divs on the page, console.logs are included to visualize data for now

--- src/components/AllPuppies.jsx
+++ src/components/AllPuppies.jsx
@@ -2,8 +2,26 @@ import React from "react";
 
 const AllPuppies = (props) => {
   const myPuppies = props.puppyData;
+  console.log(myPuppies)
 
-  return <div className="box"> {`This is the AllPuppies Component`}</div>;
-};
+
+  return (
+  <div className="box"> {`This is the AllPuppies Component`}
+    {myPuppies.length ?
+      myPuppies.map((puppy) => {
+      console.log(puppy)
+        return (
+          <div key={`player-${puppy.id}`} className = "puppy">
+            <div>{puppy.name}</div>
+            <div>{puppy.breed}</div>
+            <div>{puppy.status}</div>
+          </div>
+        )
+    })
+  :  <div>Loading the cutest puppies...</div>
+  }
+  </div>
+  )
+}
 
 export default AllPuppies;

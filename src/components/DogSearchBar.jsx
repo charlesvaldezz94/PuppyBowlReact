c92c3af6f4f60bdf76f0f02f074c3b4b3dc36280
charlesvaldezz94 <charlesvaldezz94@gmail.com>
2022-10-14 13:04:32 -0500

    updated the search bar, it now reads the value of what is written inside of it.

--- src/components/DogSearchBar.jsx
+++ src/components/DogSearchBar.jsx
@@ -3,9 +3,15 @@ import React, { useState } from "react";
 const DogSearchbar = (props) => {
   const myPuppies = props.puppyData;
 
+  const [SearchPuppies, setSearchPuppies] = useState(myPuppies);
+
+  const onSearchChange = (value) => {
+    console.log(value)
+  }
+
   function handleSearchClick(event) {
     let SearchResult = "";
-    
+
     console.log("clicked");
     event.preventDefault();
   }
@@ -14,11 +20,14 @@ const DogSearchbar = (props) => {
     <form id="SearchBar">
       <label>
         Name
-        <input type="text" />
+        <input 
+        type="search" 
+        placeholder="Type the dog's name..."
+        onChange={e=>onSearchChange(e.target.value)}
+         />
       </label>
       <button className="SearchButton" onClick={handleSearchClick}>
-        {" "}
-        Search{" "}
+        Search
       </button>
     </form>
   );

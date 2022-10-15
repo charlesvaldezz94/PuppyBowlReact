743d8ed56b70c8e497c8f792eb97a7f92e361a49
Kaylan Thompson <kaylanthompson@icloud.com>
2022-10-14 20:43:48 -0500

    Added a title to the navbar,  changed the color of the background, and changed the font size.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Navbar = () => {
   
-  return <div id="Navbar">Navbar</div>;
+  return <div id="Navbar">Welcome To Puppy Bowl 2022</div>;
+
 };
 
 export default Navbar;
